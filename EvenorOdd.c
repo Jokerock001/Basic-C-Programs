652793db3494a2e499e471246652b952273c3734
Jokerock001 <77232563+Jokerock001@users.noreply.github.com>
2021-08-25 09:59:13 -0400

    Add files via upload

---
+++ EvenorOdd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n Enter any number:");
+scanf("%d",&a);
+if((a%2)==0)
+{
+printf("\n The given number is a even number");
+}
+else
+{
+printf("\n The given number is an odd number");
+}
+getch();
+}
