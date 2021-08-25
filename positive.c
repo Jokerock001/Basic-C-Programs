78a9debe0c7ee60d97ee8b3c9fbaa40d9e32b2e9
Jokerock001 <77232563+Jokerock001@users.noreply.github.com>
2021-08-25 10:11:10 -0400

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n Enter any number:");
+scanf("%d",&a);
+if(a>0)
+{
+printf("\n The entered number is positive");
+}
+else
+{
+printf("\n The entered number is negative");
+}
+getch();
+}
+
